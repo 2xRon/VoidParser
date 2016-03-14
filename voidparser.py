ed8704324ab06ea906de2c2287ea9978a4fe34e0
Byron Smith <smith.byronx@gmail.com>
2016-03-14 18:50:04 -0400

    bugfixes
    first working version with voidplayer

--- voidparser.py
+++ voidparser.py
@@ -6,8 +6,9 @@ class VoidGame(object):
 	date = ""
 	length = 0
 	winner = ""
+	playerStats = {}
 	def __init__(self, gameID =0, names=[], score=[], kda=[], creep=[], team=[], date=0, length=0 ,winner ="", players=[]):
-		# dict of dict of game-specific info
+		# dict of dict of game-specific info (is this the best way to store this data?)
 		for id in range(len(names)):
 			temp = {"playername": names[id],"zscore": score[id], "killscore": kda[id], "creepscore": creep[id], "teamname": team[id] }
 			self.playersDict.update({names[id]: temp})
@@ -15,8 +16,8 @@ class VoidGame(object):
 		self.date   = date
 		self.length = length
 		self.winner = winner
-		# dict of player personal statistics
-		playerstats = {x.playername:x for x in players}
+		# dict of player personal statistics for the players in the game
+		self.playerStats = {x.playername:x for x in players}
 		
 	def getter(self, playername, property="playername"):
 		return self.playersDict[playername][property]
@@ -26,10 +27,10 @@ class VoidGame(object):
 		return list(self.playersDict.keys())
 		
 	def teamscore(self, playername=False):
-		sentinelTS = sum(x.zscore for x in playerstats.keys() if playersdict[x.playername]["teamname"] == 0)/5
-		scourgeTS = sum(x.zscore for x in playerstats.keys() if playersdict[x.playername]["teamname"] == 1)/5
+		sentinelTS = sum(x.zscore for x in self.playerStats.values() if self.playersDict[x.playername]["teamname"] == 0)/5
+		scourgeTS = sum(x.zscore for x in self.playerStats.values() if self.playersDict[x.playername]["teamname"] == 1)/5
 		if playername:
-			if playersDict[playername][teamname]:
+			if self.playersDict[playername]["teamname"] == 0:
 				return (sentinelTS, scourgeTS)
 			else:
 				return (scourgeTS, sentinelTS)
@@ -84,7 +85,7 @@ def playerNumberStringParse(numberString):
 
 def getGamePlayers(gamePage):
 	players = list()
-	for playerAndRealm in re.findall('playername=([a-zA-Z\.\_\(\)\-]+)\&amp\;realm\=(\w)',gamePage):
+	for playerAndRealm in re.findall('playername=([a-zA-Z0-9\.\_\(\)\-]+)\&amp\;realm\=(\w)',gamePage):
 		playerPage = getPlayerPage(playerAndRealm[0], playerAndRealm[1])
 		players.append(parsePlayerPage(playerPage,playerAndRealm[1]))
 	return players
@@ -92,7 +93,7 @@ def getGamePlayers(gamePage):
 def parseGamePage(gamePage, gameID):
 
 	# game info
-	names = re.findall('(?<=playername=)\w+',gamePage)
+	names = re.findall('(?<=playername=)[a-zA-Z0-9\.\_\(\)\-]+',gamePage)
 	score = list(map(float,re.findall('[\+|\-]\d{1}\.\d{3}', gamePage)))
 	numberString = re.findall('\d+\|\d+\|\d+',gamePage)
 	kdaString = numberString[0::2]
@@ -112,10 +113,9 @@ def parseGamePage(gamePage, gameID):
 	
 
 def parsePlayerPage(playerPage, realm):
-	playername = playername = re.findall('Init\(\'([a-zA-Z\.\_\(\)\-]+)\',\'',playerPage)[0]
-	zscore = list(map(float,re.findall('[\+|\-]\d{1}\.\d{3}', playerPage)))[0]
+	playername = playername = re.findall('Init\(\'([a-zA-Z0-9\.\_\(\)\-]+)\',\'',playerPage)[0]
+	zscore = list(map(float,re.findall('\"\>([\+|\-]\d{1}\.\d{3}|0)\<\/span\>', playerPage)))[0]
 	ngames = int(re.findall('Total Games: (\d+)',playerPage)[0])
-
 	numberString = re.findall('\d+.\d/\d+.\d/\d+.\d',playerPage)
 	kdaString = numberString[0]
 	creepString = numberString[1]
@@ -124,23 +124,23 @@ def parsePlayerPage(playerPage, realm):
 
 	winpercent = int(re.findall('(\d+)\%',playerPage)[1])
 	staypercent = int(re.findall('(\d+)\%',playerPage)[2])
-	#status = re.findall('blah',playerPage)
 	return VoidPlayer(playername, zscore, ngames, winpercent, killscore, creepscore, staypercent, realm)
 	
 	
 def main():
-	gameIDList = parseFile(sys.argv[1])
+	#gameIDList = parseFile(sys.argv[1])
+	gameIDList = parseFile('brewgames.txt')
 	gameList = []
 	for gameID in gameIDList:
 		page = getGamePage(gameID)
 		gameList.append(parseGamePage(page, gameID))
 	scores = list()
 	for game in gameList:
-		score.append(game.teamscore(playername='ReadyToBrew'))
+		scores.append(game.teamscore(playername='ReadyToBrew'))
 
-	goodScore = statistics.mean(x[0] for x in score)
-	badScore = statistics.mean(x[1] for x in score)
-	print(goodScore,badScore)
+	goodScore = statistics.mean(x[0] for x in scores)
+	badScore = statistics.mean(x[1] for x in scores)
+	print(goodScore, badScore)
 	
 			
 		
